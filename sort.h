bad942c959156731f1e9e781353cee44796e8c25
A1921-fang <74704215+A1921-fang@users.noreply.github.com>
2020-11-26 23:46:05 +0800

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifdef sort_h
+#define sort_h
+#include<stdio.h>
+#include<stdlib.h>
+
+void out(int a[],int n);
+void bubble_sort(int a[],int n);
+void insert_sort(int a[],int n);
+void selection_sort(int a[],int n);
+void quick_sort(int a[],int n);
+
+#endif
