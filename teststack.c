43620f632f9ad73f24df02cb1d93b1aa5f840598
A1921-fang <74704215+A1921-fang@users.noreply.github.com>
2020-12-03 18:39:57 +0800

    Create teststack.c

---
+++ teststack.c
@@ -0,0 +1,17 @@
+#include"stack.h"
+int main() {
+    linkStack * stack=NULL;
+    stack=push(stack, 1);
+    stack=push(stack, 2);
+    stack=push(stack, 3);
+    stack=push(stack, 4);
+    stack=pop(stack);
+    stack=pop(stack);
+    stack=pop(stack);
+    stack=pop(stack);
+    stack=pop(stack);
+
+    sum();
+    return 0;
+
+}
